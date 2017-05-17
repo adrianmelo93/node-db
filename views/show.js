c0a1d049e8a82186b7761059747b08af2b5c26e0
Adrian Melo <amelo2@fvi.edu>
2017-05-16 22:23:08 -0400

    finish the node

--- views/show.js
+++ views/show.js
@@ -1,28 +1,38 @@
 $(document).ready(function(){
-    var $name= $('#name');
-    var $craft= $('#craft');
-    var $data= $('#data');
-    var $container= $('#container');
-
     getdata();
     function getdata(search){
+
         $.ajax({
-            url:"http://api.open-notify.org/astros.json",
+            url:"/stocks",
             dataType:"json",
             method:'get',
             success: function(response){
-                console.log(response);
-                $.each(response.people, function(index,people) {
-                    var $tr = $('<tr/>');
-                     $tr.append($('<td/>').html(people.name+" "+people.craft));
-                      $tr.append($('<td/>'));
-                       $('.list-order tr:last').before($tr);
-                });
+            $('#data-table').html("");
+            response.forEach(function(value){
+                $('#data-table').append(`<tr class="gf"><td class="gf">${value.name}</td>+<td class="gf">${value.price}</td>+<td class="gf">${value.symbol}</td>+<td class="gf"><button class='button_del' id='button_del'idendb='${value._id}'>DELETE</button></td></tr>`)
 
+            })
             }
         });
 
-    }
+    };
+
+    $('#data-table').on('click', '.button_del', function (e){
+        e.preventDefault();
+       var del = $(this).attr('idendb');
+
+      $.ajax({
+          url:'/stocks/'+del,
+          method:'DELETE',
+          success: function(res){
+              $("gf").remove(".button_del");
 
 
-})
+          },
+          data:{
+              id: del
+          }
+      })
+  })
+
+});
